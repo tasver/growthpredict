6ec697d023a5af48e256a8faef6a2345eb6cedc3
tasver <bohdannavrotskyi@gmail.com>
2021-11-16 19:09:32 +0200

    heroku deployment changes6

--- growthpredict/settings.py
+++ growthpredict/settings.py
@@ -11,8 +11,8 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 import os
 import re
 
-#uri = os.getenv("DATABASE_URL")  # or other relevant config var
-#if uri and uri.startswith("postgres://"):
-#    uri = uri.replace("postgres://", "postgresql://", 1)
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 #db_uri = environ.get('SQLALCHEMY_DATABASE_URI')
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
