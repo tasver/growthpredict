fa20b6c0e9b9cc9f98a0efaabad7fa4ee6edf2e1
tasver <bohdannavrotskyi@gmail.com>
2021-11-16 17:47:25 +0200

    heroku deployment changes2

--- growthpredict/settings.py
+++ growthpredict/settings.py
@@ -6,8 +6,13 @@ SECRET_KEY = os.urandom(32)
 #app.config['SECRET_KEY'] = SECRET_KEY
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+import os
+import re
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 #db_uri = environ.get('SQLALCHEMY_DATABASE_URI')
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+engine = create_engine(uri, echo=True)
